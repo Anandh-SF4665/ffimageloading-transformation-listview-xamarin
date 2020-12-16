da6a50724af943726fe3ab997f661e9fb328f920
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-16 16:37:51 +0530

    XAMARIN-38340 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,53 @@
+﻿using FFImageLoading.Forms;
+using FFImageLoading.Transformations;
+using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<CachedImage>
+    {
+        #region Fields
+        CachedImage FFImage;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(CachedImage bindable)
+        {
+            FFImage = bindable;
+
+            var tapGesture = new TapGestureRecognizer();
+            tapGesture.Tapped += TapGesture_Tapped;
+            FFImage.GestureRecognizers.Add(tapGesture);
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(CachedImage bindable)
+        {
+            FFImage = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Call back
+        private void TapGesture_Tapped(object sender, EventArgs e)
+        {
+            var blurredTransformation = FFImage.Transformations[0] as BlurredTransformation;
+            if (blurredTransformation.Radius == 20) blurredTransformation.Radius = 0;
+            else blurredTransformation.Radius = 20;
+
+            FFImage.ReloadImage();
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using FFImageLoading.Work;
+using FFImageLoading.Transformations;
+
+namespace ListViewXamarin
+{
+    public class ViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<BookInfo> bookInfo;
+
+        #endregion
+
+        #region Constructor
+
+        public ViewModel()
+        {
+            GenerateSource();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<BookInfo> BookInfo
+        {
+            get { return bookInfo; }
+            set { this.bookInfo = value; }
+        }
+
+        #endregion
+
+        #region Generate Source
+
+        private void GenerateSource()
+        {
+            BookInfoRepository bookInfoRepository = new BookInfoRepository();
+            bookInfo = bookInfoRepository.GetBookInfo();
+        }
+        #endregion
+    }
+}
